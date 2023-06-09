4cecada8172d96afb924546bc9e16aea7e5cface
MarcusKyung <Kyungmj@gmail.com>
2023-06-09 09:05:24 -0700

    add update to NationalPark model

---
+++ AmericasParksApi/Models/Park.cs
@@ -0,0 +1,12 @@
+namespace AmericasParksApi.Models
+{
+  public class NationalPark
+  {
+    public int NationalParkId { get; set; }
+    public string Name { get; set; }
+    public string Location { get; set; }
+    public string DateEstablished { get; set; }
+    public string Area { get; set; }
+    public string Description { get; set; }
+  }
+}
