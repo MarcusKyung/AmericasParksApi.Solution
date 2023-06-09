d6b163ece43ca103cc15946cb887a0267b0c1e99
MarcusKyung <Kyungmj@gmail.com>
2023-06-09 09:49:23 -0700

    add update to Park model, park seed data

--- AmericasParksApi/Models/Park.cs
+++ AmericasParksApi/Models/Park.cs
@@ -1,9 +1,10 @@
 namespace AmericasParksApi.Models
 {
-  public class NationalPark
+  public class Park
   {
-    public int NationalParkId { get; set; }
+    public int ParkId { get; set; }
     public string Name { get; set; }
+    public string Type { get; set; }
     public string Location { get; set; }
     public string DateEstablished { get; set; }
     public string Area { get; set; }
